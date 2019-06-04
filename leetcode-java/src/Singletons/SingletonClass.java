f1d3e468d20eb4eb445ad70a433c313527ad0663
kevinphillips <inquiries@kevinphillips.email>
2019-06-04 17:32:31 -0400

    Reflection Proof and Thread Safe Singletons comparision

---
+++ leetcode-java/src/Singletons/SingletonClass.java
@@ -0,0 +1,28 @@
+package Singletons;
+
+/**
+ * @author Kevin Phillips
+ * @since 6/4/19
+ */
+
+
+public class SingletonClass {
+
+    private static SingletonClass sSoleInstance;
+
+    //private constructor
+    private SingletonClass(){
+        //Prevent form the reflection api.
+        if (sSoleInstance != null){
+            throw new RuntimeException("Use getInstance() method to get the single instance of this class.");
+        }
+    }
+
+    public static SingletonClass getInstance(){
+        if (sSoleInstance == null){ //if there is no instance available... create new one
+            sSoleInstance = new SingletonClass();
+        }
+
+        return sSoleInstance;
+    }
+}
