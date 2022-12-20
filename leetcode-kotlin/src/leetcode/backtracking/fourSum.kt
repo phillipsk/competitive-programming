b67d616e5f71058e8cffdbe5f03fb4b1cdf2ea3a
Kevin Phillips <inquiries@kevinphillips.email>
2022-12-19 20:43:55 -0500

    four sum dfs

--- leetcode-kotlin/src/leetcode/backtracking/fourSum.kt
+++ leetcode-kotlin/src/leetcode/backtracking/fourSum.kt
@@ -58,6 +58,47 @@ class FourSumSolution {
         }
         return ans.map { it }
     }
+
+//    val nums = intArrayOf(1, 0, -1, 0, -2, 2)
+    fun fourSumii(nums: IntArray, target: Int): List<List<Int>> {
+        var res = mutableListOf<MutableList<Int>>()
+
+        if (nums.isEmpty()) return res
+
+        nums.sort()
+        val n = nums.size.toInt()
+//      i = 0; j = 1; left = 2; right = (size - 1)
+        var i = 0
+        var j = 0
+        while (i < n) {
+            j = i+1
+            while (j < n) {
+                var left = j+1
+                var right = n-1
+
+                while (left < right) {
+                    val cursum = nums[i]+nums[j]+nums[left]+nums[right]
+                    if (cursum == target) {
+                        res.add(mutableListOf(nums[i], nums[j], nums[left], nums[right]))
+
+                        while (left < right && nums[left] == nums[left+1]) ++left
+                        while (left < right && nums[right] == nums[right-1]) --right
+                        ++left
+                        --right
+                    } else if (cursum < target) {
+                        ++left
+                    } else {
+                        --right
+                    }
+                }
+                while (j+1 < n && nums[j] == nums[j+1]) ++j
+                ++j
+            }
+            while (i+1 < n && nums[i] == nums[i+1]) ++i
+            ++i
+        }
+        return res
+    }
 }
 
 fun main() {
@@ -66,5 +107,6 @@ fun main() {
     val output = listOf(listOf(-2, -1, 1, 2), listOf(-2, 0, 0, 2), listOf(-1, 0, 0, 1))
 
 //    println(FourSumSolution().fourSum(nums, target))
-    println(FourSumSolution().fourSumUsingTwoPointers(nums, target))
+//    println(FourSumSolution().fourSumUsingTwoPointers(nums, target))
+    println(FourSumSolution().fourSumii(nums, target))
 }
