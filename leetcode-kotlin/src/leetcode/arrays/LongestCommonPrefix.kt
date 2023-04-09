592920eab439cdc6dd4e696a8f0e0fc795b662b6
Kevin Phillips <inquiries@kevinphillips.email>
2023-04-09 07:06:34 -0400

    horizontal scanning

--- leetcode-kotlin/src/leetcode/arrays/LongestCommonPrefix.kt
+++ leetcode-kotlin/src/leetcode/arrays/LongestCommonPrefix.kt
@@ -4,17 +4,16 @@ class LongestCommonPrefix {
     fun longestCommonPrefix(strs: Array<String>): String {
         val r = mutableListOf<List<String>>()
         val m = mutableMapOf<String, Int>()
-        val mNew = mutableMapOf<String, Int>()
+        val mNew = mutableMapOf<String, MutableList<List<String>>>()
 
         strs.forEachIndexed { i, word ->
             word.forEachIndexed { j, char ->
-//                println(word.windowed(j + 1))
                 r.add(word.windowed(j + 1))
-//                mNew[word] = word.windowed(j + 1)
             }
+            mNew[word] = r
         }
 
-        r.flatten().forEach {
+        mNew.values.flatten().flatten().forEach {
             if (m.containsKey(it)) {
                 val z = m[it]
                 m[it] = z!! + 1
@@ -23,7 +22,7 @@ class LongestCommonPrefix {
             }
         }
 
-
+        val k = m
 
         val rr = m.filter {
 
@@ -33,16 +32,28 @@ class LongestCommonPrefix {
 
         return z
     }
+
+    fun longestCommonPrefixEditorialJava(strs: Array<String>): String? {
+        if (strs.isEmpty()) return ""
+        var prefix = strs[0]
+        for (i in 1 until strs.size) while (strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length - 1)
+            if (prefix.isEmpty()) {
+                return ""
+            }
+        }
+        return prefix
+    }
 }
 
 fun main() {
-        println(
-            LongestCommonPrefix().longestCommonPrefix(
-                arrayOf("flower", "flow", "flight")
-            )
-        )
     println(
-        LongestCommonPrefix().longestCommonPrefix(
+        LongestCommonPrefix().longestCommonPrefixEditorialJava(
+            arrayOf("flower", "flow", "flight")
+        )
+    )
+    println(
+        LongestCommonPrefix().longestCommonPrefixEditorialJava(
             arrayOf("dog", "racecar", "car")
         )
     )
