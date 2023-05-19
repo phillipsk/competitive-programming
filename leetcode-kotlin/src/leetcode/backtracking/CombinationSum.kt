489e31141211b22d5d84d3687d56d0097c9a5047
Kevin Phillips <inquiries@kevinphillips.email>
2023-05-19 18:15:38 -0400

    CombinationSum init

---
+++ leetcode-kotlin/src/leetcode/backtracking/CombinationSum.kt
@@ -0,0 +1,55 @@
+package backtracking
+
+import java.util.*
+
+//  candidates = [2,3,6,7], target = 7  --> Output: [[2,2,3],[7]]
+class CombinationSum {
+    private fun backtrack(
+        target: Int,
+        nums: List<Int>,
+        output: MutableList<List<Int>>,
+        subtotal: Int,
+        list: MutableList<Int>,
+        idx: Int
+    ) {
+        var sum = subtotal
+        var i = idx
+
+        if (sum == target) {
+            output.add(ArrayList(nums))
+        }
+
+        while (sum <= target) {
+            val check = ((target - sum) - nums[i]) >= 0
+            if (check) {
+                sum += nums[i]
+                list.add(nums[i])
+            } else {
+                backtrack(target, nums, output, sum, list, i + 1)
+            }
+            i++
+        }
+    }
+
+    fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
+
+        val nums = candidates.toList()
+        val output: MutableList<List<Int>> = mutableListOf()
+        val list = mutableListOf<Int>()
+        for (i in 0 until nums.size) {
+            if (target % nums[i] == 0) { // list of all the same element
+                val l = target / nums[i]
+                output.add(List(l) { nums[i] })
+            }
+        }
+
+        backtrack(target = target, nums = nums, output = output, subtotal = 0, list, idx = 0)
+
+        return output
+    }
+}
+
+fun main() {
+    println(CombinationSum().combinationSum(intArrayOf(2, 3, 6, 7), 7))
+//    println(CombinationSum().combinationSum(intArrayOf(2, 3, 5), 8))
+}
--- leetcode-kotlin/src/leetcode/backtracking/GenerateParentheses.kt
+++ leetcode-kotlin/src/leetcode/backtracking/GenerateParentheses.kt
@@ -1,9 +1,13 @@
 package backtracking
 
 class Solution {
+    fun generateParenthesis(n: Int): List<String> {
+
+        return TODO()
+    }
 }
 
 fun main() {
-//    println(Solution().generateParenthesis(3))
+    println(Solution().generateParenthesis(3))
 //    println(Solution().generateParenthesis(2))
 }
