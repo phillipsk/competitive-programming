dcf5ceaec5653d2a7bf926abf3efc1858cf90d25
Kevin Phillips <inquiries@kevinphillips.email>
2023-05-22 21:59:33 -0400

    duplicated rotation

--- leetcode-kotlin/src/leetcode/backtracking/CombinationSum.kt
+++ leetcode-kotlin/src/leetcode/backtracking/CombinationSum.kt
@@ -1,48 +1,105 @@
 package backtracking
 
-import java.util.*
-
 //  candidates = [2,3,6,7], target = 7  --> Output: [[2,2,3],[7]]
 class CombinationSum {
-    private fun backtrack(target: Int, nums: List<Int>, output: MutableList<List<Int>>, list: MutableList<Int>) {
-        var sum = 0
-        var subtotal = target
+    private fun backtrack(
+        target: Int,
+        nums: IntArray,
+        output: MutableList<List<Int>>,
+        list: MutableList<Int>,
+        idx: Int
+    ) {
+        var list = mutableListOf<Int>()
         for (i in nums.indices) {
-            if (nums[i] == subtotal) {
-                list.add(nums[i])
-                output.add(list)
-                break
-            } else if (nums[i] < subtotal) {
-                sum += nums[i]
-                subtotal -= nums[i]
-                list.add(nums[i])
-            }
-        }
+            var subtotal = target
+            var sum = 0
 
-        if (sum > 0) {
-            backtrack(subtotal, nums, output, list)
+            WhileLoop@ while (subtotal != 0) {
+
+                for (j in nums.indices) {
+                    if (subtotal in 1 until target && subtotal % nums[j] == 0 && !output.contains(listOf(subtotal))) { // list of all the same element
+                        val l = subtotal / nums[j]
+                        list.addAll(List(l) { nums[j] })
+                        if (!output.contains(list)) {
+                            output.add(list)
+                        } else {
+                            break
+                        }
+//                        subtotal = 0
+                        list = mutableListOf()
+                        break@WhileLoop
+                    }
+                }
+
+                if (list.sum() == target) {
+                    output.add(list)
+                    subtotal = 0
+                    list = mutableListOf()
+                    break
+                    // rotate array
+                    /*                    for (j in idx until nums.size) {
+                                                    val t = nums[j]
+                                                    nums[j] = nums[j - 1]
+                                                    nums[j - 1] = t
+                                                    backtrack(target = target, nums = nums, output = output, mutableListOf(), idx + 1)
+                                                }*/
+                }
+
+                //  && nums[i] <= subtotal
+                sum += nums[i]
+                list.add(nums[i])
+                subtotal -= nums[i]
+
+                if (nums.contains(subtotal) && !output.contains(listOf(subtotal))
+                //                    && nums.indexOf(subtotal) == i
+                ) { // edge case
+                    list.add(subtotal)
+                    if (list.sum() == target) {
+                        output.add(list)
+                        subtotal = 0
+                        list = mutableListOf()
+                        break
+                        // rotate array
+                        /*                        for (j in idx until nums.size) {
+                                                            val t = nums[j]
+                                                            nums[j] = nums[j - 1]
+                                                            nums[j - 1] = t
+                                                            backtrack(target = target, nums = nums, output = output, mutableListOf(), idx + 1)
+                                                        }*/
+                    }
+                }
+
+                if (subtotal < nums.min()) {
+                    break
+                }
+
+            }
+            // rotate array
+            /*            val t = nums[i]
+                        nums[i] = nums[i + 1]
+                        nums[i + 1] = t*/
         }
     }
 
     fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
 
-        val nums = candidates.toList()
+        val nums = candidates
         val output: MutableList<List<Int>> = mutableListOf()
         val list = mutableListOf<Int>()
-/*        for (i in 0 until nums.size) {
+        for (i in 0 until nums.size) {
             if (target % nums[i] == 0) { // list of all the same element
                 val l = target / nums[i]
                 output.add(List(l) { nums[i] })
             }
-        }*/
+        }
 
-        backtrack(target = target, nums = nums, output = output, list)
+        backtrack(target = target, nums = nums, output = output, list, 1)
 
         return output
     }
 }
 
 fun main() {
-    println(CombinationSum().combinationSum(intArrayOf(2, 3, 6, 7), 7))
-//    println(CombinationSum().combinationSum(intArrayOf(2, 3, 5), 8))
+//    println(CombinationSum().combinationSum(intArrayOf(2, 3, 6, 7), 7))
+    println(CombinationSum().combinationSum(intArrayOf(2, 3, 5), 8))
 }
