72ea3eec7ba23c62b11b6bffee5e71c5e5d180c4
Kevin Phillips <inquiries@kevinphillips.email>
2023-06-14 18:25:25 -0400

    EditorialSolution recursion

--- leetcode-kotlin/src/leetcode/backtracking/ReverseString.kt
+++ leetcode-kotlin/src/leetcode/backtracking/ReverseString.kt
@@ -17,5 +17,21 @@ class ReverseString {
 
 fun main() {
 //    println(ReverseString().printReverse(charArrayOf('h', 'e', 'l', 'l', 'o')))
-    ReverseString().printReverse(charArrayOf('h', 'e', 'l', 'l', 'o'))
+//    ReverseString().printReverse(charArrayOf('h', 'e', 'l', 'l', 'o'))
+    EditorialSolution().reverseString(charArrayOf('h', 'e', 'l','j', 'l', 'o'))
+}
+internal class EditorialSolution {
+    private fun helper(s: CharArray, left: Int, right: Int) {
+        var left = left
+        var right = right
+        if (left >= right) return
+        val tmp = s[left]
+        s[left++] = s[right]
+        s[right--] = tmp
+        helper(s, left, right)
+    }
+
+    fun reverseString(s: CharArray) {
+        helper(s, 0, s.size - 1)
+    }
 }
