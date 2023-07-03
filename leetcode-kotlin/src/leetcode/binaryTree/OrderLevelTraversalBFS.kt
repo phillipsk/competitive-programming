0a7beaf0622e534a49164bbcc672e681bb3c53bc
Kevin Phillips <inquiries@kevinphillips.email>
2023-07-03 11:41:44 -0400

    BFS order level traversal

---
+++ leetcode-kotlin/src/leetcode/binaryTree/OrderLevelTraversalBFS.kt
@@ -0,0 +1,41 @@
+package binaryTree
+
+
+class Solution {
+    var levels: MutableList<MutableList<Int>> = ArrayList()
+
+    fun helper(node: TreeNode?, level: Int) {
+        // start the current level
+        if (levels.size == level) {
+            levels.add(ArrayList()) // change to new level
+        }
+
+        // fulfil the current level
+        levels[level].add(node!!.`val`)
+
+        // process child nodes for the next level
+        if (node.left != null) {
+            helper(node.left, level + 1) // change to new level
+        }
+        if (node.right != null) {
+            helper(node.right, level + 1) // change to new level
+        }
+    }
+
+    fun levelOrder(root: TreeNode?): List<MutableList<Int>> {
+        if (root == null) return levels
+        helper(root, 0)
+        return levels
+    }
+}
+
+
+fun main() {
+    var root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right!!.left = TreeNode(15)
+    root.right!!.right = TreeNode(7)
+
+    println(Solution().levelOrder(root))
+}
