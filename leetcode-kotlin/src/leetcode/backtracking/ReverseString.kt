a1306025adc73d8f562f45bfc5526ed353919e5e
Kevin Phillips <inquiries@kevinphillips.email>
2023-07-08 15:02:22 -0400

    clear solution

--- leetcode-kotlin/src/leetcode/backtracking/ReverseString.kt
+++ leetcode-kotlin/src/leetcode/backtracking/ReverseString.kt
@@ -1,37 +1,13 @@
 package backtracking
 
 class ReverseString {
+    fun reverseString(s: CharArray): Unit {
 
-    fun printReverse(str: CharArray) {
-        helper(0, str)
-    }
-
-    private fun helper(index: Int, str: CharArray?) {
-        if (str == null || index >= str.size) {
-            return
-        }
-        helper(index + 1, str)
-        print(str[index])
     }
 }
 
 fun main() {
-//    println(ReverseString().printReverse(charArrayOf('h', 'e', 'l', 'l', 'o')))
+    println(ReverseString().printReverse(charArrayOf('h', 'e', 'l', 'l', 'o')))
 //    ReverseString().printReverse(charArrayOf('h', 'e', 'l', 'l', 'o'))
-    EditorialSolution().reverseString(charArrayOf('h', 'e', 'l','j', 'l', 'o'))
-}
-internal class EditorialSolution {
-    private fun helper(s: CharArray, left: Int, right: Int) {
-        var left = left
-        var right = right
-        if (left >= right) return
-        val tmp = s[left]
-        s[left++] = s[right]
-        s[right--] = tmp
-        helper(s, left, right)
-    }
-
-    fun reverseString(s: CharArray) {
-        helper(s, 0, s.size - 1)
-    }
+//    EditorialSolution().reverseString(charArrayOf('h', 'e', 'l','j', 'l', 'o'))
 }
