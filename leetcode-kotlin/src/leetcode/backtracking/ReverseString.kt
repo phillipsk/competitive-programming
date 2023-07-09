7df16fb38794fd584c6408827e9ccbb918230597
Kevin Phillips <inquiries@kevinphillips.email>
2023-07-09 18:36:19 -0400

    my solution for ReverseString.kt

--- leetcode-kotlin/src/leetcode/backtracking/ReverseString.kt
+++ leetcode-kotlin/src/leetcode/backtracking/ReverseString.kt
@@ -2,12 +2,20 @@ package backtracking
 
 class ReverseString {
     fun reverseString(s: CharArray): Unit {
+        helper(s, 0, s.lastIndex)
 
+    }
+
+    private fun helper(s: CharArray, left: Int, right: Int) {
+        if (left < right) {
+            val temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            helper(s, left + 1, right - 1)
+        }
     }
 }
 
 fun main() {
-    println(ReverseString().printReverse(charArrayOf('h', 'e', 'l', 'l', 'o')))
-//    ReverseString().printReverse(charArrayOf('h', 'e', 'l', 'l', 'o'))
-//    EditorialSolution().reverseString(charArrayOf('h', 'e', 'l','j', 'l', 'o'))
+    println(ReverseString().reverseString(charArrayOf('h', 'e', 'l', 'l', 'o')))
 }
