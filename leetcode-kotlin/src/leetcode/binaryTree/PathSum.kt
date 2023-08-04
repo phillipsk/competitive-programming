26f588c8ecf99b8b6bf3adf793a8e9eaedc005c0
Kevin Phillips <inquiries@kevinphillips.email>
2023-08-04 09:36:58 -0400

    final attempt

--- leetcode-kotlin/src/leetcode/binaryTree/PathSum.kt
+++ leetcode-kotlin/src/leetcode/binaryTree/PathSum.kt
@@ -3,38 +3,44 @@ package binaryTree
 
 class PathSum {
     class Solution {
-        var tempSumM = 0
-        var tempSumA = mutableListOf<Int>()
+        var tempSum = 0
         var hasTargetSum = false
         var targetSumM = 0
         fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
             targetSumM = targetSum
-            root?.let { helper(it) }
+            root?.let { helper(it, root.`val`, false) }
             return hasTargetSum
         }
 
-        private fun helper(node: TreeNode) {
+        private fun helper(node: TreeNode, rootNode: Int, isEndOfLeaf: Boolean) {
+//            val isEndOfLeaf = node.left == null && node.right == null
 
-            val remain = targetSumM - tempSumM
-            val shouldAdd = if (remain > 0) {
+            val remain = if (targetSumM >= 0) {
+                targetSumM - tempSum
+            } else {
+                targetSumM + tempSum
+            }
+            val shouldAdd = if (remain >= 0) {
                 node.`val` <= remain && !hasTargetSum
             } else {
                 node.`val` >= remain && !hasTargetSum
             }
             if (shouldAdd) {
-                tempSumM += node.`val`
-//                tempSumA.add(node.`val`)
-                if (tempSumM == targetSumM && node.left == null && node.right == null) {
+                tempSum += node.`val`
+                if (tempSum == targetSumM) {
                     hasTargetSum = true
                 }
             }
 
             if (node.left != null) {
-                helper(node.left!!)
-            }
+                helper(node.left!!, rootNode, node.right == null)
+            } // else if
             if (node.right != null) {
-                helper(node.right!!)
+                helper(node.right!!, rootNode, node.left == null)
             }
+            if (isEndOfLeaf && !hasTargetSum) {
+                tempSum = rootNode
+            } // TODO: else
         }
     }
 }
@@ -63,7 +69,7 @@ fun main() {
     root = TreeNode(1)
     root.left = TreeNode(-2)
     root.right = TreeNode(3)
-    println(PathSum.Solution().hasPathSum(root, -2))
+//    println(PathSum.Solution().hasPathSum(root, -2))
 
     root = TreeNode(1)
     root.left = TreeNode(-2)
@@ -73,5 +79,27 @@ fun main() {
 
     root.right = TreeNode(-3)
     root.right!!.left = TreeNode(-2)
-//    println(PathSum.Solution().hasPathSum(root, 2))
+    println(PathSum.Solution().hasPathSum(root, -4))
 }
+
+/*
+* Testcases
+*
+[5,4,8,11,null,13,4,7,2,null,null,null,1]
+22
+[1,2,3]
+5
+[]
+0
+[-2,null,-3]
+-3
+[1,-2,3]
+-2
+[1,-2,-3,1,3,-2,null,-1]
+2
+[1,-2,-3,1,3,-2,null,-1]
+1
+[1,-2,-3,1,3,-2,null,-1]
+-1
+*
+* */
