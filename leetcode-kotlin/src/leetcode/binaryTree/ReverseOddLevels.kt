30ad943980efdb76a5651ea6ee7031d06a2d3cac
Kevin Phillips <inquiries@kevinphillips.email>
2023-08-22 21:21:37 -0400

    basic passing and before adding levels

--- leetcode-kotlin/src/leetcode/binaryTree/ReverseOddLevels.kt
+++ leetcode-kotlin/src/leetcode/binaryTree/ReverseOddLevels.kt
@@ -2,22 +2,27 @@ package binaryTree
 
 class ReverseOddLevels {
     class Solution {
-        private var shouldReverse = false
+        private var shouldReverse = true
 
         fun reverseOddLevels(root: TreeNode?): TreeNode? {
             if (root != null) {
-                helper(root)
+                helper(root.left, root.right)
             }
             return root
         }
 
-        private fun helper(root: TreeNode) {
-            if (shouldReverse) {
-                InvertBinaryTree.Solution().invertTree(root)
+        private fun helper(t1: TreeNode?, t2: TreeNode?) {
+            if (shouldReverse && t1 != null && t2 != null) {
+                val tmp = t1.`val`
+                t1.`val` = t2.`val`
+                t2.`val` = tmp
             }
             shouldReverse = !shouldReverse
-            root.left?.let { helper(it) }
-            root.right?.let { helper(it) }
+            if (t1 != null && t2 != null) {
+                helper(t1.left, t2.left)
+                helper(t1.right, t2.right)
+                shouldReverse = !shouldReverse
+            }
         }
     }
 }
@@ -26,6 +31,28 @@ fun main() {
     var root = TreeNode(7)
     root.left = TreeNode(13)
     root.right = TreeNode(11)
+
+//    root.printString()
+//    ReverseOddLevels.Solution().reverseOddLevels(root)?.printString()
+
+    root = TreeNode(2)
+    root.left = TreeNode(3)
+    root.left!!.left = TreeNode(7)
+    root.left!!.left!!.left = TreeNode(19)
+    root.left!!.left!!.right = TreeNode(23)
+
+    root.left!!.right = TreeNode(11)
+    root.left!!.right!!.left = TreeNode(29)
+    root.left!!.right!!.right = TreeNode(31)
+
+    root.right = TreeNode(5)
+    root.right!!.left = TreeNode(13)
+    root.right!!.left!!.left = TreeNode(37)
+    root.right!!.left!!.right = TreeNode(41)
+
+    root.right!!.right = TreeNode(17)
+    root.right!!.right!!.left = TreeNode(43)
+    root.right!!.right!!.right = TreeNode(47)
 
     root.printString()
     ReverseOddLevels.Solution().reverseOddLevels(root)?.printString()
