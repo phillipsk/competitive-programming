86b1ba311a99034f49820983ed0bec405333954a
Kevin Phillips <inquiries@kevinphillips.email>
2023-11-07 12:38:05 -0500

    editorials added; only change 3 lines of code

--- HackerRank/src/main/java/oneWeekPrepKit/day3/ZigZagSequence.kt
+++ HackerRank/src/main/java/oneWeekPrepKit/day3/ZigZagSequence.kt
@@ -1,7 +1,7 @@
 package src.main.java.oneWeekPrepKit.day3
 
 
-fun findZigZagSequence(arr: Array<Int>): Array<Int> {
+fun findZigZagSequenceI(arr: Array<Int>): Array<Int> {
 //    arr.sort()
     for (i in 0 until arr.size / 2) {
         val tmp = arr[i]
@@ -10,6 +10,50 @@ fun findZigZagSequence(arr: Array<Int>): Array<Int> {
     }
     return arr
 }
+
+fun findZigZagSequenceIII(a: Array<Int>): Array<Int> {
+    a.sort()
+    return ((a.slice(0 until a.size / 2) + a[a.size - 1] + a.slice(a.size / 2 until a.size - 1).reversed()).toTypedArray())
+}
+
+fun findZigZagSequenceII(arr: Array<Int>): Array<Int> {
+    arr.sort() // Sort the input array in ascending order
+
+    // Swap adjacent elements starting from the second element
+    for (i in 1 until arr.size - 1 step 2) {
+        val temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp
+    }
+
+    return arr
+}
+
+fun findZigZagSequence(arr: Array<Int>): Array<Int> {
+    // Sort the input array in ascending order
+    val sortedArr = arr.sortedArray()
+
+    // Divide the sorted array into two halves
+    val n = sortedArr.size
+    val firstHalf = sortedArr.slice(0 until n / 2)
+    val secondHalf = sortedArr.slice(n / 2 until n)
+
+    // Interleave the elements from the first and second halves
+    val zigzagSequence = mutableListOf<Int>()
+
+    for (i in 0 until n / 2) {
+        zigzagSequence.add(firstHalf[i])
+        zigzagSequence.add(secondHalf[i])
+    }
+
+    // If the input array has an odd number of elements, add the middle element
+    if (n % 2 == 1) {
+        zigzagSequence.add(sortedArr[n / 2])
+    }
+
+    return zigzagSequence.toTypedArray()
+}
+
 
 fun main(args: Array<String>) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
