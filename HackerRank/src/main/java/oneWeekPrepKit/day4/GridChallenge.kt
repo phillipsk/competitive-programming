199bbd5532a846e5f7c440b4da5de878b1ba1743
Kevin Phillips <inquiries@kevinphillips.email>
2023-11-22 12:32:59 -0500

    wip

--- HackerRank/src/main/java/oneWeekPrepKit/day4/GridChallenge.kt
+++ HackerRank/src/main/java/oneWeekPrepKit/day4/GridChallenge.kt
@@ -10,26 +10,31 @@ package src.main.java.oneWeekPrepKit.day4
 fun gridChallenge(grid: Array<String>): String {
     // Write your code here
     var isAlphaDesc = true
-    val size = grid.size
-    val list = Array(size) { "" }
-    var tmp: String
-    for (i in 0 until size) {
-        tmp = grid[i].toCharArray().sorted().joinToString("")
+    val numRows = grid.size
+    val numCols = grid[0].length
+    val list = Array(numCols) { "" }
+    for (i in 0 until numRows) {
+        val tmp = grid[i].toCharArray().sorted().joinToString("")
         grid[i] = tmp
 //        list[i] = tmp
-        for (j in 0 until size) {
-//            l.add(grid[i][j])
-            list[j] += grid[i][j]
 
-            list[j].toCharArray()[i] = grid[i][j]
-            tmp = grid[i]
+        for (j in 0 until numCols) {
+//            l.add(grid[i][j])
+            var t1 = ""
+            t1 += grid[i][j]
+            list[j] += t1
+
+//            list[j] += grid[i][j]
+
+//            list[j].toCharArray()[i] = grid[i][j]
+//            tmp = grid[i]
 
 //            val tmp = j .toCharArray().sorted().joinToString("")
 
-//            isAlphaDesc = tmp == tmp.toCharArray().sorted().joinToString("")
-//            if (!isAlphaDesc) {
-//                break
-//            }
+            isAlphaDesc = t1 == t1.toCharArray().sorted().joinToString("")
+            if (!isAlphaDesc) {
+                break
+            }
         }
     }
     println(list.joinToString())
@@ -38,9 +43,9 @@ fun gridChallenge(grid: Array<String>): String {
 
 fun main(args: Array<String>) {
 
-    val grid = arrayOf("eabcd", "fghij", "olkmn", "trpqs", "xywuv")
-//    val grid = arrayOf("mpxz", "abcd", "wlmf")
-
+//    val grid = arrayOf("eabcd", "fghij", "olkmn", "trpqs", "xywuv")
+    val grid = arrayOf("mpxz", "abcd", "wlmf")
+// maf,
     val result = gridChallenge(grid)
 
     println(result)
