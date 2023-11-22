e05cac1e10206bb2c4170e6f6a95d9d9de388acd
Kevin Phillips <inquiries@kevinphillips.email>
2023-11-22 13:21:51 -0500

    my version peer reviewed

--- HackerRank/src/main/java/oneWeekPrepKit/day4/GridChallenge.kt
+++ HackerRank/src/main/java/oneWeekPrepKit/day4/GridChallenge.kt
@@ -9,44 +9,46 @@ package src.main.java.oneWeekPrepKit.day4
 
 fun gridChallenge(grid: Array<String>): String {
     // Write your code here
-    var isAlphaDesc = true
+
+//    val grid = transpose(grid)
     val numRows = grid.size
+//    val numCols = numRows - 1
     val numCols = grid[0].length
-    val list = Array(numCols) { "" }
-    for (i in 0 until numRows) {
+
+    for (i in 0 until numCols - 1) {
         val tmp = grid[i].toCharArray().sorted().joinToString("")
         grid[i] = tmp
-//        list[i] = tmp
-
-        for (j in 0 until numCols) {
-//            l.add(grid[i][j])
-            var t1 = ""
-            t1 += grid[i][j]
-            list[j] += t1
-
-//            list[j] += grid[i][j]
-
-//            list[j].toCharArray()[i] = grid[i][j]
-//            tmp = grid[i]
-
-//            val tmp = j .toCharArray().sorted().joinToString("")
-
-            isAlphaDesc = t1 == t1.toCharArray().sorted().joinToString("")
-            if (!isAlphaDesc) {
-                break
-            }
+        for (j in 0 until numCols - 1) {
+            if (grid[j][i] > grid[j + 1][i]) return "NO"
         }
     }
-    println(list.joinToString())
-    return if (isAlphaDesc) "YES" else "NO"
+
+    return "YES"
+}
+
+fun transpose(grid: Array<String>): Array<String> {
+    val numRows = grid.size
+    val numCols = grid[0].length
+    val transposedGrid = Array(numCols) { "" }
+
+    for (i in 0 until numRows) {
+        for (j in 0 until numCols) {
+            transposedGrid[j] += grid[i][j].toString()
+        }
+    }
+
+    return transposedGrid
 }
 
 fun main(args: Array<String>) {
 
 //    val grid = arrayOf("eabcd", "fghij", "olkmn", "trpqs", "xywuv")
-    val grid = arrayOf("mpxz", "abcd", "wlmf")
-// maf,
+    val grid = arrayOf("abc", "hjk", "mpq", "rtv")
+//    val grid = arrayOf("mpxz", "abcd", "wlmf")
     val result = gridChallenge(grid)
+//    val result = transpose(grid).joinToString()
+// maw, pbl, xcm, zdf // unsorted
+// maf, pbl, xcm, zdw // sorted
 
     println(result)
 }
