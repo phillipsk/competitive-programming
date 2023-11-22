358f7d4e01954b63f9a1b6dad6c201a74e4ad36d
Kevin Phillips <inquiries@kevinphillips.email>
2023-11-22 17:00:26 -0500

    incorrect loop through all permutations

---
+++ HackerRank/src/main/java/arrays/CompareTriplets.kt
@@ -0,0 +1,34 @@
+package src.main.java.arrays
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
+    val list = Array(2) { 0 }
+    for (i in a.indices) {
+        for (j in b.indices) {
+            if (a[i] > b[i]) list[0]++
+            else if (a[i] < b[i]) list[1]++
+//            val eq = a[i] == b[j]
+            println("a ${a[i]} :: b ${b[j]} //")
+        }
+    }
+    return list
+}
+
+fun main(args: Array<String>) {
+
+    val a = arrayOf(5, 6, 7)
+
+    val b = arrayOf(3, 6, 10)
+
+    val result = compareTriplets(a, b)
+
+    println(result.joinToString(" "))
+}
